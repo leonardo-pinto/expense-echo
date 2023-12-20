f23fa21b3717fc23fa003fb53f32780512ab7844
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-12-20 14:10:02 -0500

    feat: add UserController

---
+++ backend/src/main/java/com/backend/expenseecho/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.backend.expenseecho.controller;
+
+import com.backend.expenseecho.security.UserInfoUserDetails;
+import com.backend.expenseecho.model.entities.UserInfo;
+import com.backend.expenseecho.model.dto.AuthResponseDto;
+import com.backend.expenseecho.model.dto.LoginRequestDto;
+import com.backend.expenseecho.model.dto.RegisterRequestDto;
+import com.backend.expenseecho.security.JwtTokenProvider;
+import com.backend.expenseecho.service.impl.UserInfoServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+    private final UserInfoServiceImpl userInfoService;
+    private final JwtTokenProvider jwtService;
+    private final AuthenticationManager authManager;
+
+    public UserController(UserInfoServiceImpl userInfoService, JwtTokenProvider jwtService, AuthenticationManager authManager) {
+        this.userInfoService = userInfoService;
+        this.jwtService = jwtService;
+        this.authManager = authManager;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDto> register(@Valid @RequestBody RegisterRequestDto request) {
+        // TODO: map dto to entity
+        UserInfo user = new UserInfo(request.getFirstName(), request.getLastName(), request.getEmail(), request.getPassword());
+        UserInfo createdUser = userInfoService.login(user);
+        String token = jwtService.generateToken(createdUser.getEmail(), createdUser.getId().toString());
+        return new ResponseEntity<>(new AuthResponseDto(token), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDto> login(@Valid @RequestBody LoginRequestDto request) {
+        try {
+            Authentication authentication = authManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+            );
+            UserInfoUserDetails userDetails = (UserInfoUserDetails) authentication.getPrincipal();
+            String token = jwtService.generateToken(userDetails.getUsername(), userDetails.getId().toString());
+            return new ResponseEntity<>(new AuthResponseDto(token), HttpStatus.OK);
+        } catch (BadCredentialsException ex) {
+            System.out.println("Bad credentials exception");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    @GetMapping("/test")
+    public String testMethod() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
+    }
+
+}
