c48f74a9667a9950222afea3c187cbe21eae6686
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-12-26 17:13:03 -0500

    feat: add profile entity

---
+++ backend/src/main/java/com/backend/expenseecho/model/entities/Profile.java
@@ -0,0 +1,59 @@
+package com.backend.expenseecho.model.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "profiles")
+public class Profile {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String name;
+    private String avatar;
+
+    public Profile() {
+    }
+
+    public Profile(String name, String avatar, User user) {
+        this.user = user;
+        this.name = name;
+        this.avatar = avatar;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
