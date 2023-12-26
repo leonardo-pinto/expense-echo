8d1b0da56ed5bc98ba03df4885cf770ff8b806cd
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-12-26 17:16:54 -0500

    feat: add ProfileService implementation

---
+++ backend/src/main/java/com/backend/expenseecho/service/impl/ProfileServiceImpl.java
@@ -0,0 +1,72 @@
+package com.backend.expenseecho.service.impl;
+
+import com.backend.expenseecho.exception.BadRequestException;
+import com.backend.expenseecho.exception.ResourceNotFoundException;
+import com.backend.expenseecho.exception.UnauthorizedException;
+import com.backend.expenseecho.model.dto.CreateProfileRequest;
+import com.backend.expenseecho.model.dto.ProfileResponse;
+import com.backend.expenseecho.model.dto.UpdateProfileRequest;
+import com.backend.expenseecho.model.entities.Profile;
+import com.backend.expenseecho.model.entities.User;
+import com.backend.expenseecho.repository.ProfileRepository;
+import com.backend.expenseecho.repository.UserInfoRepository;
+import com.backend.expenseecho.service.ProfileService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProfileServiceImpl implements ProfileService {
+
+    private ProfileRepository profileRepository;
+    private UserInfoRepository userRepository;
+
+    public ProfileServiceImpl(ProfileRepository profileRepository, UserInfoRepository userRepository) {
+
+        this.profileRepository = profileRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Profile create(CreateProfileRequest request, String userId) {
+        User user = userRepository.findById(Integer.parseInt(userId)).orElseThrow(() -> new UsernameNotFoundException("User account not found"));
+        Profile profile = new Profile(request.getName(), request.getAvatar(), user);
+
+        if (profileNameAlreadyExists(request.getName(), profile.getUser().getId())) {
+            throw new BadRequestException("Profile name already exists.");
+        }
+
+        return profileRepository.save(profile);
+
+    }
+
+    @Override
+    public List<ProfileResponse> getAllByUserId(String userId) {
+        List<Profile> profiles = profileRepository.findByUserId(Integer.parseInt(userId));
+
+        return profiles.stream().map(ProfileResponse::new).toList();
+    }
+
+    @Override
+    public ProfileResponse updateProfile(String id, UpdateProfileRequest request, String userId) {
+        Profile profile = profileRepository.findById(Integer.parseInt(id)).orElseThrow(() -> new ResourceNotFoundException("Profile not found."));
+
+        if (!profile.getUser().getId().toString().equals(userId)) {
+            throw new UnauthorizedException("User is not authorized to perform this action.");
+        }
+        if (profileNameAlreadyExists(request.getName(), profile.getUser().getId())) {
+            throw new BadRequestException("Profile name already exists.");
+        }
+
+        profile.setName(request.getName());
+        profile.setAvatar(request.getAvatar());
+
+        Profile updatedProfile = profileRepository.save(profile);
+        return new ProfileResponse(updatedProfile);
+    }
+
+    public Boolean profileNameAlreadyExists(String profileName, int userId) {
+        return profileRepository.findByNameAndUserId(profileName, userId).isPresent();
+    }
+}
