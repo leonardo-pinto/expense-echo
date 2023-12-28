5e5d1254a2ae04c703c32c6f44b2272855d91bb8
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-12-28 10:14:49 -0500

    chore: update imports

--- backend/src/main/java/com/backend/expenseecho/service/AuthService.java
+++ backend/src/main/java/com/backend/expenseecho/service/AuthService.java
@@ -2,7 +2,6 @@ package com.backend.expenseecho.service;
 
 import com.backend.expenseecho.model.dto.RegisterRequest;
 import com.backend.expenseecho.model.dto.RegisterUserResponse;
-import com.backend.expenseecho.model.entities.User;
 
 public interface AuthService {
     RegisterUserResponse register(RegisterRequest registerRequest);
