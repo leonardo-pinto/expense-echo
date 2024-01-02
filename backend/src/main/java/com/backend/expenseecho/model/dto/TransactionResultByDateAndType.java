95ec5bc47410ecf689b7db8c3f8dbf0adf72a8cf
leonardo-pinto <leonardoan.pinto@gmail.com>
2024-01-02 16:28:53 -0500

    feat: add Transaction dtos

---
+++ backend/src/main/java/com/backend/expenseecho/model/dto/TransactionResultByDateAndType.java
@@ -0,0 +1,35 @@
+package com.backend.expenseecho.model.dto;
+
+import com.backend.expenseecho.model.enums.TransactionType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+import java.math.BigDecimal;
+
+public class TransactionResultByDateAndType {
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+
+    public TransactionResultByDateAndType(BigDecimal totalAmount, TransactionType type) {
+        this.totalAmount = totalAmount;
+        this.type = type;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+}
